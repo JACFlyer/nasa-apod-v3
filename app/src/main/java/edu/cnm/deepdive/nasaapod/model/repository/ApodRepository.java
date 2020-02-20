3f1a568698f2cac010626ead84bd0837d5cace1f
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-02-20 10:58:15 -0700

    Starting to add download functionality.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/model/repository/ApodRepository.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/model/repository/ApodRepository.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.nasaapod.model.repository;
 
 import android.app.Application;
+import android.os.Environment;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
@@ -8,19 +9,22 @@ import edu.cnm.deepdive.nasaapod.model.dao.AccessDao;
 import edu.cnm.deepdive.nasaapod.model.dao.ApodDao;
 import edu.cnm.deepdive.nasaapod.model.entity.Access;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
+import edu.cnm.deepdive.nasaapod.model.entity.Apod.MediaType;
 import edu.cnm.deepdive.nasaapod.model.pojo.ApodWithStats;
 import edu.cnm.deepdive.nasaapod.service.ApodDatabase;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
 import io.reactivex.Single;
 import io.reactivex.SingleSource;
 import io.reactivex.schedulers.Schedulers;
+import java.io.File;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ApodRepository {
+public class  ApodRepository {
 
   private static final int NETWORK_THREAD_COUNT = 10;
 
@@ -29,6 +33,10 @@ public class ApodRepository {
   private final Executor networkPool;
 
   private static Application context;
+
+  private static final Pattern URL_FILENAME_PATTERN = Pattern.compile("^.*/([^/#?]+)(?:\\?.*)?(?:#.*)?$");
+
+  private static final String LOCAL_FILENAME_FORMAT = "%1$tY%1$tm%1$td-%2$s";
 
   private ApodRepository() {
     if (context == null) {
@@ -54,6 +62,7 @@ public class ApodRepository {
         .switchIfEmpty((SingleSource<? extends Apod>) (observer) ->
             nasa.get(BuildConfig.API_KEY, ApodService.DATE_FORMATTER.format(date))
                 .subscribeOn(Schedulers.from(networkPool))
+
                 .flatMap((apod) ->
                     dao.insert(apod)
                         .map((id) -> {
@@ -72,8 +81,26 @@ public class ApodRepository {
 
   public Single<String> getImage(@NonNull Apod apod) {
     // TODO Add local file download & reference.
+    boolean canBeLocal = (apod.getMediaType() == MediaType.IMAGE);
+    File file = canBeLocal ? getFile(apod) : null;
     return Single.fromCallable(apod::getUrl);
   }
+
+  private File getFile(@NonNull Apod apod) {
+    String url = apod.getUrl();
+    File file = null;
+    Matcher matcher = URL_FILENAME_PATTERN.matcher(url);
+    if (matcher.matches()) {
+      String filename = String.format(LOCAL_FILENAME_FORMAT, apod.getDate(), matcher.group(1));
+      File directory = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+      if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState(directory))) {
+        directory = context.getFilesDir();
+      }
+      file = new File(directory, filename);
+    }
+    return file;
+  }
+
 
   private void insertAccess(Apod apod) {
     AccessDao accessDao = database.getAccessDao();
